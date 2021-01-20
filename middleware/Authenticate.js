f7f6d1fda2f33dc440beb8c4fa8b66c685c7c327
alikaakati <kaakatiali@gmail.com>
2021-01-20 22:15:42 +0200

    fixed

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -21,7 +21,7 @@ authenticateModerator = (req,res,next) =>{
 }
 
 authenticateUser = (req , res , next) =>{
-    let filter = {username : username , token : token};
+    let filter = {username : req.body.username , token : req.body.token};
     Account.find(filter , (err , doc) =>{
         if(err) return res.json({error : 'error'});
         else next();
